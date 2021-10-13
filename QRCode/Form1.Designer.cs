b1db531601bc4481af0f8c8e3678dc1b7ba10758
Manuel Bento <manuelbentomb.223@hotmail.com>
2021-10-13 11:00:20 +0100

    Add project files.

---
+++ QRCode/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace QRCode_Front
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(12, 58);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(430, 380);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "excel.png");
+            this.imageList1.Images.SetKeyName(1, "png-clipart-pdf-adobe-acrobat-others-template-angle-thumbnail.png");
+            this.imageList1.Images.SetKeyName(2, "transferir.png");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(557, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(221, 205);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(418, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(24, 24);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(400, 24);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(458, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Gerar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(557, 321);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(221, 117);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(792, 94);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 205);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 467);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ QRCode/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using QRCoder;
+
+namespace QRCode_Front
+{
+    public partial class Form1 : Form
+    {
+        QRCodeGenerator gera = new QRCodeGenerator();
+
+        string selected;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            ListViewItem listViewItem;
+            FileInfo fileInfo;
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowser.SelectedPath;
+                foreach (var item in Directory.GetFiles(textBox1.Text))
+                {
+                    fileInfo = new FileInfo(item);
+                    if (fileInfo.Extension == ".xlsx")
+                    {
+                        listViewItem = new ListViewItem();
+                        listViewItem.Text = fileInfo.Name;
+                        listViewItem.SubItems.Add(fileInfo.FullName);
+                        listViewItem.ImageIndex = 0;
+                        listView1.Items.Add(listViewItem);
+                    }
+
+                    else if (fileInfo.Extension == ".pdf")
+                    {
+                        listViewItem = new ListViewItem();
+                        listViewItem.Text = fileInfo.Name;
+                        listViewItem.SubItems.Add(fileInfo.FullName);
+                        listViewItem.ImageIndex = 1;
+                        listView1.Items.Add(listViewItem);
+                    }
+                    if (fileInfo.Extension == ".docx")
+                    {
+                        listViewItem = new ListViewItem();
+                        listViewItem.Text = fileInfo.Name;
+                        listViewItem.SubItems.Add(fileInfo.FullName);
+                        listViewItem.ImageIndex = 0;
+                        listView1.Items.Add(listViewItem);
+                    }
+                }
+                if(listView1.Items.Count==0)
+                    MessageBox.Show("Não foram encontrados tipos de arquivo suportado!","Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+                return;
+            selected = listView1.SelectedItems[0].SubItems[1].Text;
+        }
+
+        private void Gerar(string url)
+        {
+            richTextBox1.Clear();
+            /*QRCodeData dataCode = gera.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            QRCode codigo = new QRCode(dataCode);
+            Bitmap imageCode = codigo.GetGraphic(20);
+
+            /*
+            MemoryStream ms = new MemoryStream();
+            imageCode.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            Byte[] arquivoDesfeito = ms.ToArray();
+
+            pictureBox1.Image = Image.FromStream(ms);
+            richTextBox1.Text = Convert.ToBase64String(arquivoDesfeito);
+            Stream stream = new MemoryStream(arquivoDesfeito);
+            pictureBox2.Image = Image.FromStream(stream);*/
+
+            //Convertendo o arquivo selecionado em stream
+            FileStream file = new FileStream(url,FileMode.Open,FileAccess.Read);
+            MemoryStream stream = new MemoryStream();
+            file.CopyTo(stream);
+  
+            
+            Byte[] arquivoByte = stream.ToArray();
+            richTextBox1.Text = Convert.ToBase64String(arquivoByte);
+
+            //Convertendo o arquivo em Byte
+            //pictureBox2.Image = Image.FromStream(stream);
+
+            //Gerando o QR Code com a url
+            QRCodeData dataCode = gera.CreateQrCode(url,QRCodeGenerator.ECCLevel.Q);
+            QRCode codigo = new QRCode(dataCode);
+            Bitmap imageCode = codigo.GetGraphic(20);
+
+            MemoryStream ms = new MemoryStream();
+            imageCode.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            pictureBox1.Image = Image.FromStream(ms);
+            //pictureBox2.Image = Image.FromStream(stream);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (selected != "")
+                Gerar(selected);
+        }
+    }
+}
